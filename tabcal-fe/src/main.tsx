10ba4f9edee6b2fa9e163142836eba36f10e4419
Toby <toby.lau075@gmail.com>
2024-07-02 15:05:33 -0700

    added disclaimer

--- tabcal-fe/src/main.tsx
+++ tabcal-fe/src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import Navbar from './components/Navbar.tsx'
 import { Toaster } from './components/UI/Toast/Toaster.tsx'
+import Disclaimer from './components/Disclaimer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="flex flex-col md:flex-row ">
       <Navbar className={"w-screen h-18 border-b-[1px] md:w-28 md:h-screen md:border-r-[1px] border-black"}/>
+      <Disclaimer />
       <App />
       <Toaster />
     </div>
