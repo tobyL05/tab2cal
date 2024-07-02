9a94095c78683022e499db01c3332a8a038f985b
Toby <toby.lau075@gmail.com>
2024-07-02 13:59:02 -0700

    tuned model

--- tabcal-be/routes/upload/gemini.py
+++ tabcal-be/routes/upload/gemini.py
@@ -7,12 +7,6 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
-
-API_KEY = os.getenv("API_KEY")
-
-genai.configure(api_key=API_KEY)
-model = genai.GenerativeModel('gemini-1.5-pro-latest')
-
 def get_prompt():
     prompt = os.getenv("GEMINI_PROMPT")
     curr_date = date.today().strftime("%Y/%m/%d")
@@ -21,6 +15,22 @@ def get_prompt():
     # print(parsed)
     return parsed
 
+
+API_KEY = os.getenv("API_KEY")
+generation_config = {
+  "temperature": 0.3,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+genai.configure(api_key=API_KEY)
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+    system_instruction=get_prompt()
+)
 
 def parse_img(img) -> str:
     try:
