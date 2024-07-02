9d47955fe2f4d281780fb8031a117a504369d11d
Toby <toby.lau075@gmail.com>
2024-07-02 12:33:53 -0700

    added about dialog

--- tabcal-fe/src/main.tsx
+++ tabcal-fe/src/main.tsx
@@ -8,7 +8,7 @@ import { Toaster } from './components/UI/Toast/Toaster.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div className="flex flex-col md:flex-row ">
-      <Navbar className={"w-screen h-18 border-b-[1px] md:w-28 md:h-screen md:border-r-[1px] border-black "}/>
+      <Navbar className={"w-screen h-18 border-b-[1px] md:w-28 md:h-screen md:border-r-[1px] border-black"}/>
       <App />
       <Toaster />
     </div>
